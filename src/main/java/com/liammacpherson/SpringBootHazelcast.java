a388c484c20a836da217101e00e42fe583c34e6e
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2021-07-22 21:54:36 +0100

    Add basic spring boot application
    Add basic spring boot application that includes the spring boot 
    starter web dependancy. This change also includes a change to the 
    .gitignore to include all of the gradle and .idea artefacts.

---
+++ src/main/java/com/liammacpherson/SpringBootHazelcast.java
@@ -0,0 +1,13 @@
+package com.liammacpherson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHazelcast {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHazelcast.class, args);
+    }
+
+}
